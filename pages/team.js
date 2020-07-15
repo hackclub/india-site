65c02957b276890de80395bba7bc642adb0e6825
bajpai244 <bajpaiharsh244@gmail.com>
2020-07-15 22:56:32 +0530

    added teams pag

---
+++ pages/team.js
@@ -0,0 +1,38 @@
+/**@jsx jsx*/
+
+import { jsx, Grid } from 'theme-ui'
+import Header from '../components/Header/index'
+import Intro from '../components/Team/Introduction/index'
+import Card from '../components/Team/card'
+import Footer from '../components/Footer/index'
+
+export default () => (
+  <div sx={{ width: '100%' }}>
+    <Header />
+    <Intro />
+    <Grid columns={[1, 1, 2]} sx={{ pb: 5, mt: 5 }}>
+      <Card
+        url={'athul'}
+        name="Athul Blesson"
+        position="Regional Director at Hack Club"
+      />
+      <Card
+        url={'annlee'}
+        name="Annlee Fores"
+        position="COO at Hack Club APAC"
+      />
+      <Card
+        url={'arun'}
+        name="Arun Mani"
+        position="CTO at Hack Club APAC
+"
+      />
+      <Card
+        url={'thejaswi'}
+        name="Thejaswi Prakash"
+        position="Content & Social media at Hack Club APAC"
+      />
+    </Grid>
+    <Footer />
+  </div>
+)
